142ab9f41b7894bc4b7fc1a62c54e48e91691c6d
saba1122333 <tchumburidzesaba@gmail.com>
2025-06-20 12:00:49 +0400

    Initial Commit

---
+++ src/AJIP/Main.java
@@ -0,0 +1,6 @@
+package AJIP;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("AJIP side!");
+    }
+}
---
+++ src/JBTP/Main.java
@@ -0,0 +1,7 @@
+package JBTP;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("JBTP world!");
+    }
+}
